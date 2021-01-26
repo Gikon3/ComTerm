9d9ecb1099ca608dd2ec39938f09e39aea766bd4
BiBi <13802189+Gikon3@users.noreply.github.com>
2021-01-26 09:17:56 +0300

    add work ports

--- ComTerm.cpp
+++ ComTerm.cpp
@@ -1,11 +1,17 @@
 #include "ComTerm.h"
 #include "ui_ComTerm.h"
+#include "TabForm.h"
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QMessageBox>
 
 ComTerm::ComTerm(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::ComTerm)
 {
     ui->setupUi(this);
+    ui->tabWidget->hide();
+    updateFreePortList();
 }
 
 ComTerm::~ComTerm()
@@ -13,3 +19,58 @@ ComTerm::~ComTerm()
     delete ui;
 }
 
+// slots
+void ComTerm::scanButtonClicked()
+{
+    updateFreePortList();
+}
+
+void ComTerm::connectButtonClicked()
+{
+    if(QSerialPort *port = openPort(ui->portsListComboBox->currentText())) {
+        TabForm *tab = new TabForm;
+        ui->tabWidget->addTab(tab, port->portName());
+        openPortsList.insert(TabPortPair(tab, port));
+        connect(port, &QSerialPort::readyRead, this, &ComTerm::handleReadyRead);
+        if(ui->tabWidget->isHidden()) {
+            ui->tabWidget->show();
+        }
+    }
+}
+// -- slots
+
+void ComTerm::updateFreePortList()
+{
+    QString curPort = ui->portsListComboBox->currentText();
+    ui->portsListComboBox->clear();
+    for(const QSerialPortInfo& port: QSerialPortInfo().availablePorts()) {
+        if(!port.isBusy()) {
+            ui->portsListComboBox->addItem(port.portName());
+        }
+    }
+    ui->portsListComboBox->setCurrentText(curPort);
+}
+
+QSerialPort* ComTerm::openPort(const QString& portName)
+{
+    QSerialPort *port = new QSerialPort();
+    port->setPortName(portName);
+    port->setBaudRate(QSerialPort::Baud115200);
+    port->setDataBits(QSerialPort::Data8);
+    port->setFlowControl(QSerialPort::NoFlowControl);
+    port->setParity(QSerialPort::NoParity);
+    port->setStopBits(QSerialPort::OneStop);
+    if(port->open(QSerialPort::ReadWrite)) {
+        return port;
+    }
+    delete port;
+    QMessageBox::warning(this, "Warning", "Port " + portName + " is open");
+    return nullptr;
+}
+
+void ComTerm::handleReadyRead()
+{
+    for(const TabPortPair& tabPort: openPortsList) {
+        tabPort.first->append(tabPort.second->readAll());
+    }
+}
--- ComTerm.h
+++ ComTerm.h
@@ -2,6 +2,9 @@
 #define COMTERM_H
 
 #include <QMainWindow>
+#include <QSet>
+#include <QSerialPort>
+#include "TabForm.h"
 
 QT_BEGIN_NAMESPACE
 namespace Ui { class ComTerm; }
@@ -15,7 +18,18 @@ public:
     ComTerm(QWidget *parent = nullptr);
     ~ComTerm();
 
+private slots:
+    void scanButtonClicked();
+    void connectButtonClicked();
+
 private:
+    void updateFreePortList();
+    QSerialPort* openPort(const QString& portName);
+    void handleReadyRead();
+
+private:
+    using TabPortPair = QPair<TabForm*, QSerialPort*>;
     Ui::ComTerm *ui;
+    QSet<TabPortPair> openPortsList;
 };
 #endif // COMTERM_H
